46201782a0551267d0becca4e9f4167651e9f29b
Dimuthu1994 <dimuthudiss6@gmail.com>
2021-11-30 14:53:35 +0530

    mui styling1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { Button } from "@mui/material";
+import styled from "@emotion/styled";
+import DeleteIcon from "@mui/icons-material/Delete";
+
+const MyButton = styled(Button)({
+  color: "white",
+  backgroundColor: "black",
+});
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <MyButton variant="contained" color="error" startIcon={<DeleteIcon />}>
+        Material ui
+      </MyButton>
+    </div>
+  );
 }
 
 export default App;
