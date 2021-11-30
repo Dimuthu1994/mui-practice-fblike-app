f845057a6b904a4f2282928bd791f8888f6e194e
Dimuthu1994 <dimuthudiss6@gmail.com>
2021-11-30 19:33:02 +0530

    mui styling4

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import { Button } from "@mui/material";
-import styled from "@emotion/styled";
-import DeleteIcon from "@mui/icons-material/Delete";
-import { purple } from "@mui/material/colors";
-const MyButton = styled(Button)({
-  color: "white",
-  backgroundColor: purple[800],
+import Button from "@mui/material/Button";
+import { styled } from "@mui/system";
 
-  "&:hover": {
-    backgroundColor: purple[500],
-  },
+const MyComponent = styled("div")({
+  color: "darkslategray",
+  backgroundColor: "aliceblue",
+  padding: 8,
+  borderRadius: 4,
+});
+
+const MyButton = styled(Button)({
+  background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+  border: 0,
+  borderRadius: 3,
+  boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+  color: "white",
+  height: 48,
+  padding: "0 30px",
 });
 
 function App() {
   return (
-    <div>
-      <MyButton variant="contained" startIcon={<DeleteIcon />}>
-        Material ui
-      </MyButton>
-    </div>
+    <>
+      <MyComponent>Styled div</MyComponent>
+      <MyButton>Styled Components</MyButton>
+    </>
   );
 }
 
