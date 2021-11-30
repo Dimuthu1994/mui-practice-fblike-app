d34d6143cd69219045d10a9162640d54f72171ef
Dimuthu1994 <dimuthudiss6@gmail.com>
2021-11-30 17:02:13 +0530

    mui styling2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import { Button } from "@mui/material";
-import styled from "@emotion/styled";
+import { styled } from "@mui/styles";
 import DeleteIcon from "@mui/icons-material/Delete";
 
 const MyButton = styled(Button)({
   color: "white",
-  backgroundColor: "black",
+  backgroundColor: "#212121",
+
+  "&:hover": {
+    backgroundColor: "#616161",
+  },
 });
 
 function App() {
   return (
     <div>
-      <MyButton variant="contained" color="error" startIcon={<DeleteIcon />}>
+      <MyButton variant="contained" startIcon={<DeleteIcon />}>
         Material ui
       </MyButton>
     </div>
